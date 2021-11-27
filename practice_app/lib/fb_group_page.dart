697b3903d56c41a7bdde0f21e9556153b943b53b
aditya995 <adityashowrov1996@gmail.com>
2021-11-28 01:53:02 +0600

    changes

--- practice_app/lib/fb_group_page.dart
+++ practice_app/lib/fb_group_page.dart
@@ -13,14 +13,57 @@ class FbGroupPage extends StatelessWidget {
           children: [
             CircleAvatar(
               backgroundImage: AssetImage('files/images/fb.png'),
-              radius: 150,
             ),
-            IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.search_rounded,
-                  color: Colors.black,
-                )),
+            SizedBox(
+              width: 10,
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.black12,
+              child: IconButton(
+                  splashRadius: 20,
+                  hoverColor: Colors.transparent,
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.search_rounded,
+                    color: Colors.black54,
+                  )),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            FloatingActionButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              backgroundColor: Colors.transparent,
+              foregroundColor: Colors.black54,
+              hoverColor: Colors.black12,
+              focusColor: Colors.black12,
+              splashColor: Colors.white,
+              elevation: 0,
+              hoverElevation: 0,
+              focusElevation: 0,
+              highlightElevation: 0,
+              onPressed: () {},
+              child: Icon(
+                Icons.menu_rounded,
+                size: 40,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.black12,
+              child: IconButton(
+                  splashRadius: 20,
+                  hoverColor: Colors.transparent,
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.black,
+                  )),
+            ),
           ],
         ),
       ),
