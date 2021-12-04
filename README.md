aa7e6c98ad768d3e2cb32373a16f5e93e0d904e9
aditya995 <39437959+aditya995@users.noreply.github.com>
2021-12-04 19:05:44 +0600

    Asset Image Loaded from files/images/, basic layout:  MaterialApp > SafeArea > Scaffold > Container/Row/Column/singleChildScrollView

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-# flutterPractice
+# Basic Layout of app pages
 
 ## Important concepts
- - class 1: use files, App name change, Icon change, debug mark removed from app, scaffold
- - class 2:
+ - Topic 001: create app, Use files, App name change, Icon change, debug mark removed from app tip right corner, scaffold, safearea, container
+ - Topic 002: column, row
+ - Topic 003: center, align, padding
+ - Topic 003: text
+ - Topic 004: expanded, flexible, 
+ - Topic 003: singleChildScrollView
+ - Topic 003: flex, spacer
+ - Topic 003: card, sixedbox
+ - Topic 003: image
+ - Topic 003: icon
+ - Topic 003: textButton
+ - Topic 003: iconButton
+ - Topic 003: floatingActionButton
+ - Topic 003: elevatedbutton
+ - Topic 003: appbar
---
+++ basicLayout/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //  To wipe out "debug" mark in the right side of screen
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        // Excludes the area containing "phone camera" on the top of screen
+        child: Scaffold(
+          //  Implements the basic material design visual layout structure.
+          //  This class provides APIs for showing drawers and bottom sheets.
+          appBar: AppBar(
+            title: const Center(
+              child: Text("Great work!"),
+            ),
+          ),
+
+          body: Container(
+            child: Image(image: AssetImage('files/images/box.png')),
+          ),
+        ),
+      ),
+    );
+  }
+}
