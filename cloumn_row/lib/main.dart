45e94c700c321a47c62a41a78e250f494b897b5e
aditya995 <39437959+aditya995@users.noreply.github.com>
2021-12-10 01:51:11 +0600

    cloumn row

--- cloumn_row/lib/main.dart
+++ cloumn_row/lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
             children: [
               // learnColumn(),
               // learnColumn(),
+
               learnRow(),
             ],
           ),
