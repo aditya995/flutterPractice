910f9ba565101d34e8da12d95c3d778ae9547a94
aditya995 <39437959+aditya995@users.noreply.github.com>
2021-12-09 18:05:25 +0600

    Text widget

--- text/lib/main.dart
+++ text/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Text widget',
       home: SafeArea(
         child: Scaffold(
@@ -29,23 +32,30 @@ class myTextWidget extends StatelessWidget {
     return Center(
       child: Container(
         color: Colors.amber,
-        height: 400,
-        width: 200,
+        height: 650,
+        width: 300,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Text(
-              'Text Data Text Data ',
+          children: [
+            const Text(
+              'Text Data Text \nData Text \nData Text Data \nText Data Text Data Text Data Text Data Text Data Text Data Text Data Text Data ',
               style: TextStyle(
                 color: Colors.green,
                 backgroundColor: Colors.white,
-                fontSize: 25,
+                fontSize: 5,
                 fontWeight: FontWeight.w100,
                 fontStyle: FontStyle.italic,
+                height: 2,
               ),
-              // textAlign: TextAlign.end, // Better use Align() widget for this
+              overflow: TextOverflow.ellipsis,
+              maxLines: 4,
+              textScaleFactor: 2, // increases font size
+              // textHeightBehavior: TextHeightBehavior(
+              //   applyHeightToFirstAscent: true,
+              //   applyHeightToLastDescent: false,
+              // ),
             ),
-            Text(
+            const Text(
               'Text Data Text Data Text Data',
               style: TextStyle(
                 color: Colors.green,
@@ -55,8 +65,11 @@ class myTextWidget extends StatelessWidget {
                 // "wordSpacing" Should be avoided pushes out texts outside parent container area!! ****
                 wordSpacing: 35,
               ),
+              // strutStyle: StrutStyle(),
+              textDirection: TextDirection.rtl,
+              softWrap: true,
             ),
-            Text(
+            const Text(
               'Text Data Text Data\nText Data',
               style: TextStyle(
                 color: Colors.green,
@@ -69,11 +82,62 @@ class myTextWidget extends StatelessWidget {
               ),
             ),
             Text(
-              'Text Data Text Data Text Data Text Data Text Data Text Data Text Data',
+              'Text Data Text Data ',
               style: TextStyle(
-                color: Colors.white,
-                backgroundColor: Colors.blue,
+                fontSize: 55,
+                color: Colors.black,
+                backgroundColor: Colors.white,
+                // background: Paint(),
+                shadows: [
+                  Shadow(
+                      color: Colors.red,
+                      blurRadius: 1.5,
+                      offset: Offset.fromDirection(1.5, 11.5)),
+                  Shadow(
+                      color: Colors.green,
+                      blurRadius: 1.5,
+                      offset: Offset.fromDirection(1.5, 6.5))
+                ],
+                decoration: TextDecoration.combine(
+                    [TextDecoration.lineThrough, TextDecoration.underline]),
+                decorationColor: Colors.blue,
+                decorationStyle: TextDecorationStyle.dashed,
+                decorationThickness: 1,
               ),
+            ),
+            Container(
+              height: 100,
+              width: 150,
+              color: Colors.black,
+              child: const Text(
+                'Text Data Text Data Text Data Text Data Text Data\nText Data',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  // debugLabel: 'DebugLabel',
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 150,
+              decoration: BoxDecoration(
+                color: Colors.amber.withOpacity(0.3),
+                border: Border.all(color: Colors.black, width: 5),
+              ),
+              child: const Text(
+                'Text Data Text Data Text Data Text Data Text Data\nText Data',
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  // overflow: TextOverflow.ellipsis,
+                ),
+                textAlign: TextAlign.end,
+              ),
+              clipBehavior: Clip.antiAliasWithSaveLayer,
             ),
           ],
         ),
